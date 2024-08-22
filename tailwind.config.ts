4c43901da6c18342fa26856f0f3a2a60adedda63
Dries Hooghe <dries@drieshooghe.com>
2024-08-22 19:59:30 +0200

    🔧 replace tailwinds green color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+	colors: {
+		green: {
+			100: '#BAC1B6',
+			200: '#979F93',
+			300: '#6E7A66',
+			400: '#55604E',
+			500: '#434D3C',
+			600: '#313B2B',
+			700: '#1E251B',
+			800: '#0A0C09',
+			900: '#0C0F0B',
+		}
+	},
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
