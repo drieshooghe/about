59cf8af89e605d741810271e092eef51ded6a607
Dries Hooghe <dries@drieshooghe.com>
2024-08-24 18:07:26 +0200

    🍱 add SVG icons

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+export { At } from './at';
+export { Cheqroom } from './cheqroom';
+export { DASMedia } from './das-media';
+export { GitHub } from './github';
+export { LinkedIn } from './linkedin';
+export { Location } from './location';
+export { Meditech } from './meditech';
