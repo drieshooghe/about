c70c4a312b7dbf5289993a520c92da75657fd625
Dries Hooghe <dries@drieshooghe.com>
2024-08-29 13:52:02 +0200

    :sparkles: enable printing the resume page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import globalContent from '@content/global.json';
 import type { Metadata } from 'next';
 import { Outfit } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const font = Outfit({ subsets: ['latin'], variable: '--font-outfit' });
 
@@ -14,6 +15,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<Script>{`document.getElementById('print-button').addEventListener('click',function() {window.print();});`}</Script>
 			<body
 				className={`${font.variable} font-sans max-w-xs mx-auto sm:max-w-md md:max-w-2xl xl:max-w-5xl md:mx-auto print:max-w-2xl`}
 			>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,29 @@
 import indexContent from '@content/index.json';
 
-import { At, Cheqroom, DASMedia, LinkedIn, Location, Meditech } from '@assets/icons';
+import { Location, Printer } from '@assets/icons';
 import Avatar from '@components/avatar';
 import CompanyLogo from '@components/company-logo';
 import Paragraph from '@components/paragraph';
 import SectionTitle from '@components/section-title';
 import Timeline from '@components/timeline/timeline';
-import React, { ImgHTMLAttributes } from 'react';
-import { GitHub } from '../assets/icons/github';
+import React from 'react';
 
 export default function Home() {
 	const { info, introduction, heading, expertise, skills, interests, experience, education, cta } = indexContent;
 	return (
 		<>
-			<header className="flex flex-col items-center col-span-4 mt-10 text-green-600">
+			<header className="flex flex-col print:flex-row  items-center mt-10 text-green-600 print:gap-4 relative">
+				<button id="print-button" type="button" className="absolute right-0 top-0 print:hidden">
+					<Printer className="text-green-300 transition-colors w-8 h-8 hover:text-green-400" />
+				</button>
 				<Avatar />
-				<p className="pb-1 text-xl">{heading.pre}</p>
-				<h1 className="mb-3 text-4xl font-medium">{heading.title}</h1>
-				<p className="flex items-center gap-0.5">
-					<Location className="w-4 h-4" /> <span className="text-sm font-light">{info.location}</span>
-				</p>
+				<div className="flex flex-col items-center print:items-start">
+					<p className="pb-1 text-xl">{heading.pre}</p>
+					<h1 className="mb-3 text-4xl font-medium">{heading.title}</h1>
+					<p className="flex items-center gap-0.5">
+						<Location className="w-4 h-4" /> <span className="text-sm font-light">{info.location}</span>
+					</p>
+				</div>
 			</header>
 			<section className="grid grid-cols-1 py-4 mt-4 mb-8 border-y">
 				<Paragraph weight="light" size="large" className="text-center">
@@ -29,7 +33,7 @@ export default function Home() {
 			<main>
 				<div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
 					{/** Expertise */}
-					<section className="order-1 sm:order-2 grid auto-rows-min gap-y-2">
+					<section className="order-1 sm:order-2 grid auto-rows-min gap-y-2 break-inside-avoid">
 						<SectionTitle>{expertise.title}</SectionTitle>
 						<ul>
 							{expertise.items.map((item) => (
@@ -40,12 +44,12 @@ export default function Home() {
 						</ul>
 					</section>
 					{/** Highlighted experience */}
-					<section className="order-2 sm:order-1 grid auto-rows-min gap-y-2">
+					<section className="order-2 sm:order-1 grid auto-rows-min gap-y-2 break-inside-avoid">
 						<SectionTitle>{experience.title.short}</SectionTitle>
 						<Timeline experience={experience} />
 					</section>
 					{/** Skills */}
-					<section className="order-3 md:order-2 grid auto-rows-min gap-y-2">
+					<section className="order-3 md:order-2 grid auto-rows-min gap-y-2 break-inside-avoid">
 						<SectionTitle>{skills.title}</SectionTitle>
 						<ul>
 							{skills.items.map((item) => (
@@ -56,7 +60,7 @@ export default function Home() {
 						</ul>
 					</section>
 					{/** Education & Certifications */}
-					<section className="order-4 grid auto-rows-min gap-y-2">
+					<section className="order-4 grid auto-rows-min gap-y-2 break-inside-avoid">
 						<SectionTitle>{education.title}</SectionTitle>
 						<ul className="list-disc ml-4 marker:text-green-500">
 							{education.items.map((item) => (
@@ -70,7 +74,7 @@ export default function Home() {
 						</ul>
 					</section>
 					{/** Interests */}
-					<section className="order-5 grid auto-rows-min gap-y-2">
+					<section className="order-5 grid auto-rows-min gap-y-2 break-inside-avoid print:mt-16">
 						<SectionTitle>{interests.title}</SectionTitle>
 						<ul>
 							{interests.items.map((item) => (
@@ -81,12 +85,12 @@ export default function Home() {
 						</ul>
 					</section>
 					{/** Full experience */}
-					<section className="order-6 sm:col-span-2 grid auto-rows-min gap-y-4">
+					<section className="order-6 sm:col-span-2 grid auto-rows-min gap-y-4 break-inside-avoid">
 						<SectionTitle className="sm:text-center">{experience.title.full}</SectionTitle>
 						<Timeline variation="full" experience={experience} />
 					</section>
 				</div>
-				<div className="grid grid-cols-1 mt-12 ">
+				<div className="grid grid-cols-1 mt-12 break-inside-avoid print:hidden">
 					<div className="p-4 text-white bg-green-400 rounded-t-xl">
 						<SectionTitle>{cta.title}</SectionTitle>
 					</div>
