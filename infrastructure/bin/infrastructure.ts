81bdf6ed34ef1bd3ccdc71c3ed3789f79d1871d9
Dries Hooghe <dries@drieshooghe.com>
2024-09-06 14:44:13 +0200

    🚨 format the infrastructure files

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { OperationsStack } from "../lib/operations-stack";
-import { WebsiteStack } from "../lib/website-stack";
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { OperationsStack } from '../lib/operations-stack';
+import { WebsiteStack } from '../lib/website-stack';
 
 const app = new cdk.App();
 
-new OperationsStack(app, "OperationsStack");
-new WebsiteStack(app, "WebsiteStack");
+new OperationsStack(app, 'OperationsStack');
+new WebsiteStack(app, 'WebsiteStack');
