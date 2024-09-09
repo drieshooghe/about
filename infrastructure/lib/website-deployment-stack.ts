46a864b2a7b39db19182f5490668969382fe8264
Dries Hooghe <dries@drieshooghe.com>
2024-09-09 17:28:09 +0200

    :bricks: specify different browser caching directives depending on which file

--- infrastructure/lib/website-deployment-stack.ts
+++ infrastructure/lib/website-deployment-stack.ts
@@ -1,6 +1,13 @@
 import * as path from 'node:path';
+import { Duration } from 'aws-cdk-lib';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { BucketDeployment, ServerSideEncryption, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import {
+  BucketDeployment,
+  type BucketDeploymentProps,
+  CacheControl,
+  ServerSideEncryption,
+  Source,
+} from 'aws-cdk-lib/aws-s3-deployment';
 import type { Construct } from 'constructs';
 import { Stack, type StackProps } from './constructs';
 
@@ -12,10 +19,33 @@ export class WebsiteDeploymentStack extends Stack {
   }
 
   private createDeployment() {
-    new BucketDeployment(this, 'WebsiteDeployment', {
+    const settings: Omit<BucketDeploymentProps, 'sources' | 'cacheControl'> = {
       destinationBucket: Bucket.fromBucketName(this, 'WebsiteDeploymentBucket', 'drieshooghe-website'),
-      sources: [Source.asset(path.resolve(__dirname, '../../website/out'))],
       serverSideEncryption: ServerSideEncryption.AES_256,
+    };
+
+    new BucketDeployment(this, 'PageWebsiteDeployment', {
+      ...settings,
+      sources: [
+        Source.asset(path.resolve(__dirname, '../../website/out'), {
+          exclude: ['!index.html', '!404.html', '!index.txt'],
+        }),
+      ],
+      cacheControl: [CacheControl.noStore()],
+    });
+
+    new BucketDeployment(this, 'AssetWebsiteDeployment', {
+      ...settings,
+      sources: [
+        Source.asset(path.resolve(__dirname, '../../website/out'), {
+          exclude: ['index.html', '404.html', 'index.txt'],
+        }),
+      ],
+      cacheControl: [
+        CacheControl.setPublic(),
+        CacheControl.maxAge(Duration.hours(24)),
+        CacheControl.staleWhileRevalidate(Duration.hours(24)),
+      ],
     });
   }
 }
