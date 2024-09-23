85eaa2f898a62b42a1f1ffd0c60e081311395420
Dries Hooghe <dries@drieshooghe.com>
2024-09-23 10:16:58 +0200

    ✨ add a cf function to redirect apex requests to the www domain

---
+++ infrastructure/lib/functions/redirect-apex.ts
@@ -0,0 +1,32 @@
+import type { CloudFrontRequestEvent } from 'aws-lambda';
+
+const handler = (event: CloudFrontRequestEvent) => {
+  const request = event.Records[0].cf.request;
+  const headers = request.headers;
+
+  // Get the Host header from the incoming request
+  const hostHeader = headers.host?.[0].value;
+
+  // Apex domain and www subdomain
+  const apexDomain = 'drieshooghe.com';
+  const wwwDomain = 'www.drieshooghe.com';
+
+  // If the request is to the apex domain, redirect to the www domain
+  if (hostHeader === apexDomain) {
+    const redirectUrl = `https://${wwwDomain}${request.uri}`;
+
+    // Return a 301 redirect response
+    return {
+      status: '301',
+      statusDescription: 'Moved Permanently',
+      headers: {
+        location: [{ key: 'Location', value: redirectUrl }],
+        'cache-control': [{ key: 'Cache-Control', value: 'max-age=86400' }],
+      },
+    };
+  }
+
+  return request;
+};
+
+export { handler };
